45c37502d56a75c1e2d9a4a0175efaf334544815
孙元侃 <sunyuankan@sunyuankandeMac-mini.local>
2015-08-03 13:46:56 +0800

    孙元侃 首页各controller

--- mybuilding/Main/MainViewController.m
+++ mybuilding/Main/MainViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "MainViewController.h"
+#import "RKStageViewPageController.h"
+#import "RKTestController_1.h"
+#import "RKTestController_2.h"
+#import "RKTestController_3.h"
+#import "RKTestController_4.h"
+#import "RKTestController_5.h"
+#import "RKTestController_6.h"
 
 @interface MainViewController ()
-
+@property(nonatomic, strong) RKStageViewPageController* pageController;
 @end
 
 @implementation MainViewController
@@ -18,21 +25,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], NSForegroundColorAttributeName,[UIFont boldSystemFontOfSize:19], NSFontAttributeName,nil]];
-}
+    
+    RKTestController_1* vc1 = [[RKTestController_1 alloc] init];
+    RKTestController_2* vc2 = [[RKTestController_2 alloc] init];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+    RKTestController_3* vc3 = [[RKTestController_3 alloc] init];
+    RKTestController_4* vc4 = [[RKTestController_4 alloc] init];
+    RKTestController_5* vc5 = [[RKTestController_5 alloc] init];
+    RKTestController_6* vc6 = [[RKTestController_6 alloc] init];
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    NSArray* ary = @[vc1,vc2,vc3,vc4,vc5,vc6];
+    
+    self.pageController = [[RKStageViewPageController alloc] initWithTitles:@[@"1",@"2",@"3",@"4",@"5",@"6"] controllers:ary];
+    [self.view addSubview:self.pageController.view];
 }
-*/
 
 @end
